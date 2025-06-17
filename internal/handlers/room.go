c19320ca0ce71a839f3379e0d4aaad4662fcd5e2
Phpar-Ph <146304604+Phpar-Ph@users.noreply.github.com>
2025-06-17 18:46:12 +0800

    update chat.go

--- internal/handlers/room.go
+++ internal/handlers/room.go
@@ -3,12 +3,13 @@ package handlers
 import (
 	"fmt"
 	"os"
-	"time"
 
+	w "vidoechat/pgk/webrtc"
 
-    w "vidoechat/pgk/webrtc"
-	"github.com/gofiber/fiber/v3"
+	"github.com/fasthttp/websocket"
 	"github.com/gofiber/contrib/websocket"
+	"github.com/gofiber/fiber/v3"
+
 	guuid "github.com/google/uuid"
 )
 
@@ -25,7 +26,20 @@ func Room (c *fiber.Ctx) error {
 		c.Status(400)
 		return nil
 	}
-	uuid,guuid,_ := createOrGetRoom(uuid)
+	ws := "ws"
+	if os.Getenv("ENVIRONMENT") == "PRODUCTION"{
+		ws = "wss"
+	}
+	uuid,suuid,_ := createOrGetRoom(uuid)
+	return c.Render("peer", fiber.Map{
+		"RoomWebsocketAddr": fmt.Sprintf("%s://%s/room%s/websocket", ws, c.Hostname(), uuid),
+		"RoomLink": fmt.Sprintf("%s://%s/room/%s",c.Protocol(), c.Hostname(), uuid),
+		"ChatWebSocketAddr":fmt.Sprintf("%s://%s/room/%s/chat/websocket", ws, c.Hostname(), uuid),
+		"ViewerWebSocketAddr": fmt.Sprintf("%s://%s/room/%s/viewer/websocket", ws, c.Hostname(), uuid),
+		"StreamLink": fmt.Sprintf("%s://%s/steam/%s", c.Protocol(), c.Hostname(), suuid),
+		"Type": "room",
+
+	}, "layouts/main")
 }
 
 func RoomWebsocket(c *websocket.Conn){
@@ -44,8 +58,38 @@ func createOrGetRoom(uuid string)(string, string, *w.Room){
 }
 
 func RoomViewerWebsocket(c *websocket.Conn, p *w.Peers){
+      uuid := c.Params("uuid")
+	  if uuid == ""{
+		return
+	  }
 
+	  w.RoomsLock.Lock()
+	  if peer, ok := w.Rooms[uuid]; ok{
+		w.RoomsLock.Unlock()
+		roomViewerConn(c, peer.Peers)
+		return
+	  }
+	 w.RoomsLock.Unlock()
 }
+
+func roomViewerConn(c *websocket.Conn, p *w.Peers){
+	ticker :=time.NewTIcker(1*time.Second)
+	defer ticker.Stop()
+	defer c.CLose()
+
+	for{
+		select{
+		case <- ticker.C:
+			w, err := c.Conn.NexWriter(websocket.TextMessage)
+			if err != nil{
+				return
+			}
+
+			w.Write([]byte(fmt.Sprintf("%d", len(p.Connections))))
+		}
+	}
+}
+
 
 type websocketMessage struct{
 	Event string `json:"event"`
